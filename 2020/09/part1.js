3aa6d0f0f2284784e0fb734842b6ca1d0ba51fa9
Lukasz Brodowski <lukebrodowski@gmail.com>
2020-12-09 00:42:48 -0500

    2020 day 9

---
+++ 2020/09/part1.js
@@ -0,0 +1,28 @@
+function isXmas(numbers, next) {
+    for (let i = 0; i < numbers.length; i++) {
+        let low = numbers[i];
+
+        for (let j = numbers.length - 1; j > i; j--) {
+            let hi = numbers[j];
+
+            if (hi + low === next) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+module.exports = (file) => {
+    let n = file.split('\n').map(Number);
+
+    for (let i = 0; i < n.length - 25; i += 1) {
+        let code = n.slice(i, i + 25);
+        let test = n[i + 25];
+
+        if (!isXmas(code, test)) return test;
+    }
+
+    return 'did not find the answer';
+};
