f765f41eaa85d116724df16a4ce3698c66502369
Lukasz Brodowski <lukebrodowski@gmail.com>
2020-12-15 20:14:26 -0500

    convert to map for speed

--- 2020/15/part1.js
+++ 2020/15/part1.js
@@ -1,26 +1,33 @@
-const { dict } = require('../../utils');
-
-module.exports = (file) => {
+module.exports = (file, max = 2020) => {
     let plays = file.split(',').map(Number);
-    let lastSpoken = dict(() => []);
+    let lastSpoken = new Map();
 
-    plays.forEach((p, i) => lastSpoken[p].push(i + 1));
+    plays.forEach((p, i) => lastSpoken.set(p, [i + 1]));
 
-    let n = plays.length;
-    let last = plays[plays.length - 1];
+    let start = plays.length;
+    let last = plays[start - 1];
 
-    while (n < 2020) {
-        n += 1;
+    for (let n = start + 1; n <= max; n++) {
+        let spoken = lastSpoken.get(last);
 
-        if (lastSpoken[last].length > 1) {
-            let spoken = lastSpoken[last];
-            let len = spoken.length;
-            last = spoken[len - 1] - spoken[len - 2];
+        if (spoken.length === 2) {
+            last = spoken[1] - spoken[0];
         } else {
             last = 0;
         }
 
-        lastSpoken[last].push(n);
+        if (lastSpoken.has(last)) {
+            spoken = lastSpoken.get(last);
+        } else {
+            spoken = [];
+            lastSpoken.set(last, spoken);
+        }
+
+        spoken.push(n);
+
+        if (spoken.length === 3) {
+            spoken.shift();
+        }
     }
 
     return last;
--- 2020/15/part2.js
+++ 2020/15/part2.js
@@ -1,53 +1,9 @@
-const { dict, observe } = require('../../utils');
+const part1 = require('./part1');
 
-module.exports = (file) => {
-    // took 12 minutes to brute force so just skip
-    // if the input is my original
-    if (file === '2,20,0,4,1,17') return 814;
-
-    let plays = file.split(',').map(Number);
-    let lastSpoken = dict(() => []);
-
-    plays.forEach((p, i) => lastSpoken[p].push(i + 1));
-
-    // every 30s
-    // let tick = observe(30);
-
-    let n = plays.length;
-    let last = plays[plays.length - 1];
-
-    while (n < 30000000) {
-        n += 1;
-
-        if (lastSpoken[last].length === 2) {
-            let [before, now] = lastSpoken[last];
-            last = now - before;
-        } else {
-            last = 0;
-        }
-
-        lastSpoken[last].push(n);
-
-        // save on space by cleaning up old index's
-        // since we only look back two
-        if (lastSpoken[last].length > 2) {
-            lastSpoken[last] = lastSpoken[last].slice(-2);
-        }
-
-        // [1] [30s] Observed: {
-        //     "n": 5455524,
-        //     "last": 3276331
-        // }
-        // [1] [30s] Observed: {
-        //     "n": 6533453,
-        //     "last": 1302938
-        // }
-        // 30000000 / 6533453 = ~4.59
-        // so >5ish minutes to brute force if things
-        // stay linear
-        // took 12 minutes to update, def no linear
-        // tick({ n, last });
-    }
-
-    return last;
+/**
+ * Consolidsated both parts to a more efficient version.
+ * Same code for both.
+ */
+module.exports = (input) => {
+    return part1(input, 30000000);
 };
