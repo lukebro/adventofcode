776110d26d22a93c156eb012a754a814bf3d5e0b
Lukasz Brodowski <lukebrodowski@gmail.com>
2021-12-01 00:23:35 -0500

    day 1

--- 2021/01/part1.ts
+++ 2021/01/part1.ts
@@ -1,5 +1,16 @@
 export default (input: string) => {
-    const lines = input.split('\n');
+    const lines = input.split('\n').map(Number);
 
-    return '0';
+    let increases = 0;
+
+    for (let i = 1; i < lines.length; i++) {
+        const a = lines[i - 1];
+        const b = lines[i];
+
+        if (b > a) {
+            increases += 1;
+        }
+    }
+
+    return increases;
 };
--- 2021/01/part2.ts
+++ 2021/01/part2.ts
@@ -1,1 +1,18 @@
-export default (input: string) => {};
+export default (input: string) => {
+    const lines = input.split('\n').map(Number);
+
+    let increases = 0;
+    let prev = Infinity;
+
+    for (let i = 2; i < lines.length; i++) {
+        const next = lines[i - 2] + lines[i - 1] + lines[i];
+
+        if (next > prev) {
+            increases += 1;
+        }
+
+        prev = next;
+    }
+
+    return increases;
+};
