3a7a92b39530264e6c511abd2dff894e7d520126
Lukasz Brodowski <lukebrodowski@gmail.com>
2021-12-02 00:11:15 -0500

    day 2

---
+++ 2021/02/part1.ts
@@ -0,0 +1,30 @@
+import { findall } from '@lib/utils';
+
+export default (file: string) => {
+    const commands = file
+        .split('\n')
+        .map((line) => findall(/(forward|down|up) (\d+)/g, line));
+
+
+    let depth = 0;
+    let horizontal = 0;
+
+    for (const [direction, value] of commands) {
+        let inc = Number(value);
+        switch (direction) {
+
+            case 'forward':
+                horizontal += inc;
+                break;
+            case 'down':
+                depth += inc;
+                break;
+            case 'up':
+                depth -= inc;
+                break;
+        }
+    }
+
+    return depth * horizontal;
+};
+
