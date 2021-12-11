1fd05423ecf16e954cf6675309c9bf17b86ada49
Lukasz Brodowski <lukebrodowski@gmail.com>
2021-12-11 00:55:21 -0500

    count flashes per step rather than globally

--- 2021/11/part1.ts
+++ 2021/11/part1.ts
@@ -1,11 +1,10 @@
 export default (file: string) => {
     const grid = file.split('\n').map((line) => line.split('').map(Number));
-    let flashes = 0;
 
-    const flash = (x, y) => {
-        if (grid[y][x] <= 9 || grid[y][x] === 0) return;
+    const flash = (x, y): number => {
+        if (grid[y][x] <= 9 || grid[y][x] === 0) return 0;
 
-        flashes += 1;
+        let flashes = 1;
         grid[y][x] = 0;
 
         for (let dy = y - 1; dy <= y + 1; dy++) {
@@ -21,11 +20,13 @@ export default (file: string) => {
                     grid[dy][dx] += 1;
 
                     if (grid[dy][dx] === 10) {
-                        flash(dx, dy);
+                        flashes += flash(dx, dy);
                     }
                 }
             }
         }
+
+        return flashes;
     };
 
     const step = () => {
@@ -35,15 +36,20 @@ export default (file: string) => {
             }
         }
 
+        let flashes = 0;
+
         for (let y = 0; y < grid.length; y++) {
             for (let x = 0; x < grid[y].length; x++) {
-                flash(x, y);
+                flashes += flash(x, y);
             }
         }
+
+        return flashes;
     };
 
+    let flashes = 0;
     for (let i = 0; i < 100; i++) {
-        step();
+        flashes += step();
     }
 
     return flashes;
